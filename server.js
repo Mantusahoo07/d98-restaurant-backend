7ec5869ca89317cf020b4b39ca4af705abb75399
Mantusahoo07 <mantusahoo.bgr@gmail.com>
2025-12-01 14:30:27 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ const app = express();
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "20mb" }));
+app.use(express.urlencoded({ extended: true, limit: "20mb" }));
+
 
 // Database connection
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/d98-restaurant', {
