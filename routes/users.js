a37ab072bd209fb2533787958f9bc2f943083ccf
Mantusahoo07 <mantusahoo.bgr@gmail.com>
2025-12-02 19:35:22 +0530

    Add files via upload

--- routes/users.js
+++ routes/users.js
@@ -3,14 +3,21 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 const auth = require('../middleware/auth');
 
+// All routes protected by Firebase Token middleware
 router.use(auth);
 
+// ✔ CREATE USER (Required by frontend)
+router.post('/', userController.createUserProfile);
+
+// ✔ GET USER PROFILE
 router.get('/profile', userController.getUserProfile);
+
+// ✔ UPDATE USER PROFILE
 router.put('/profile', userController.updateUserProfile);
 
-// Address routes
-router.post('/addresses', userController.addAddress);
-router.put('/addresses/:addressId', userController.updateAddress);
-router.delete('/addresses/:addressId', userController.deleteAddress);
+// Address routes (fixed names to match frontend)
+router.post('/address', userController.addAddress);
+router.put('/address/:addressId', userController.updateAddress);
+router.delete('/address/:addressId', userController.deleteAddress);
 
 module.exports = router;
