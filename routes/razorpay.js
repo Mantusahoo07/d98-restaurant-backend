bb8b42485eee5d4751341b10967c38c1130e3e0a
Mantusahoo07 <mantusahoo.bgr@gmail.com>
2025-12-03 19:38:38 +0530

    Add files via upload

--- routes/razorpay.js
+++ routes/razorpay.js
@@ -5,7 +5,7 @@ const Razorpay = require('razorpay');
 
 // Initialize Razorpay
 const razorpay = new Razorpay({
-    key_id: process.env.RZP_KEY_ID || 'rzp_test_dr3j7aeO5e1ItX',
+    key_id: process.env.RZP_KEY_ID,
     key_secret: process.env.RZP_KEY_SECRET
 });
 
