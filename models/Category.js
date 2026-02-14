4bd68e03516c9205457a622eddd0c86005de843c
Mantusahoo07 <mantusahoo.bgr@gmail.com>
2026-02-14 10:56:05 +0530

    Update Category.js

--- models/Category.js
+++ models/Category.js
@@ -1,16 +1,33 @@
 const mongoose = require('mongoose');
 
 const CategorySchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-    unique: true
-  },
-  enabled: {
-    type: Boolean,
-    default: true
-  }
-}, { timestamps: true });
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        default: ''
+    },
+    image: {
+        type: String,
+        default: 'https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80'
+    },
+    icon: {
+        type: String,
+        default: 'fas fa-tag'
+    },
+    enabled: {
+        type: Boolean,
+        default: true
+    },
+    displayOrder: {
+        type: Number,
+        default: 0
+    }
+}, {
+    timestamps: true
+});
 
 module.exports = mongoose.model('Category', CategorySchema);
