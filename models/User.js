5cc50f20faee7a4107584b0d854b63e6b3c42b23
Mantusahoo07 <mantusahoo.bgr@gmail.com>
2026-02-21 19:29:07 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -35,6 +35,8 @@ const userSchema = new mongoose.Schema({
     unique: true
   },
   phone: String,
+  photoURL: String,  // Add this
+  coverURL: String,  // Add this
   addresses: [addressSchema],
   preferences: {
     notifications: {
